c8852ffc0b8d15b5f5b64bd03bcd76dbb05808a9
Bella Barnabás <barnabasbella@gmail.com>
2024-05-03 07:29:35 +0200

    endpoints created

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+
+@app.post('/move-mouse')
+def move_mouse():
+    x = request.args.get('x', type=int)
+    y = request.args.get('y', type=int)
+    return
+
+
+@app.post('/type-character')
+def type_character():
+    char = request.args.get('char', type=str)
+    return
+
+
+@app.post('/click')
+def send_click():
+    return
+
+
+@app.get('/click')
+def click_response():
+    return
