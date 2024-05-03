5718b7848bc0266f8cef75e279a681cc7ef49ef8
Bella Barnabás <barnabasbella@gmail.com>
2024-05-03 11:18:18 +0200

    basic move mouse working

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
+import socket
 from flask import Flask
 from flask import request
 from flask import render_template
+import pyautogui
 
 app = Flask(__name__)
 
 
+def get_ipv4_address():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as st:
+        st.connect(("8.8.8.8", 80))
+        ipv4_address = st.getsockname()[0]
+    return ipv4_address
+
+
+ip_address = get_ipv4_address()
+port = 5000
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', ip_address=ip_address, port=port)
 
 
-@app.post('/move-mouse')
+@app.get('/move-mouse')
 def move_mouse():
     x = request.args.get('x', type=int)
     y = request.args.get('y', type=int)
-    return
+    # moveRel(xOffset=x, yOffset=y)
+    pyautogui.moveTo(x, y)
+    return f"x: {x} , y: {y}"
 
 
 @app.post('/type-character')
@@ -31,3 +46,7 @@ def send_click():
 @app.get('/click')
 def click_response():
     return
+
+
+if __name__ == "__main__":
+    app.run(host=ip_address, port=port)
