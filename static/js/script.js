41dee9867f68a542bacb42ce0147281fbf6d58c8
Bella Barnabás <barnabasbella@gmail.com>
2024-05-05 04:32:34 +0200

    code cosmetic changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,65 +1,64 @@
 const postMoveMouse = function (x, y) {
-    const endpoint = "/move-mouse"
+    const endpoint = "/move-mouse";
 
-    let data = new FormData()
-    data.append("x", x)
-    data.append("y", y)
+    let data = new FormData();
+    data.append("x", x);
+    data.append("y", y);
 
     fetch(endpoint, {
         "method": "POST",
         "body": data,
-    })
-}
+    });
+};
 
 const postTypeCharacter = function (character) {
-    const endpoint = "/type-character"
+    const endpoint = "/type-character";
 
-    let data = new FormData()
-    data.append("character", character)
+    let data = new FormData();
+    data.append("character", character);
 
     fetch(endpoint, {
         "method": "POST",
         "body": data,
-    })
-}
+    });
+};
 
 const postClick = function (button) {
-    const endpoint = "/click"
+    const endpoint = "/click";
 
-    let data = new FormData()
-    data.append("click", button)
+    let data = new FormData();
+    data.append("click", button);
 
     fetch(endpoint, {
         "method": "POST",
         "body": data,
-    })
-}
+    });
+};
 
-
-function startup() {
-    const keyboard = document.getElementById("keyboard")
-    const touchpad = document.getElementById("touchpad")
+const startup = function () {
+    const keyboard = document.getElementById("keyboard-input");
+    const touchpad = document.getElementById("touchpad");
 
     touchpad.addEventListener("touchstart", handleStart);
     touchpad.addEventListener("touchend", handleEnd);
     touchpad.addEventListener("touchcancel", handleCancel);
     touchpad.addEventListener("touchmove", handleMove);
-}
+};
 
 document.addEventListener("DOMContentLoaded", startup);
 
 let previousCoordinates = null;
 
-function handleStart(evt) {
+const handleStart = function (evt) {
     evt.preventDefault();
     const touches = evt.changedTouches;
 
     for (let i = 0; i < touches.length; i++) {
         previousCoordinates = { x: Math.trunc(touches[i].clientX), y: Math.trunc(touches[i].clientY) };
     }
-}
+};
 
-function handleMove(evt) {
+const handleMove = function (evt) {
     evt.preventDefault();
     const touches = evt.changedTouches;
     const id = touches[0].identifier;
@@ -71,15 +70,15 @@ function handleMove(evt) {
             previousCoordinates = currentCoordinates;
         }
     }
-}
+};
 
-function handleEnd(evt) {
+const handleEnd = function (evt) {
     evt.preventDefault();
     previousCoordinates = null;
-}
+};
 
-function handleCancel(evt) {
+const handleCancel = function (evt) {
     evt.preventDefault();
     previousCoordinates = null;
-}
+};
 
